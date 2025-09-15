b3b3afa7e3a18381815d1b8a4b9f6782792c053c
Marek Sokol <mareksokol98@gmail.com>
2025-09-16 00:34:04 +0200

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,8 +1,21 @@
+import models.mautner.model as Mautner
+from processing import process_mautner
+
+
+# TODO: Implement logic to handle different exercises
 class Exercise:
-    def __init__(self):
-        pass
+    def __init__(self, name=None):
+        self.name = name
+
+        # Models
+        self.wide_squat_model = Mautner.ModelInterface()
 
     def describe(self):
         return {}
 
-# TODO: Implementovat model pro hodnocení Wide squat - https://github.com/MautnerVo/Hydronaut
+    def predict_wide_squat_phase(self, data):
+        # Process input data according to model requirements
+        processed_data = process_mautner(data)
+
+        # Make prediction
+        return self.wide_squat_model.predict(processed_data)
