fbbe6ddd1a925442c6c486cf2b52e76c4b66b87f
HonzaHejda <88653738+HonzaHejda@users.noreply.github.com>
2025-10-07 03:35:29 +0200

    New model support

--- models/mautner/model.py
+++ models/mautner/model.py
@@ -1,24 +1,27 @@
 import torch
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-
+import pickle
+import os
 
 class Net(nn.Module):
-    def __init__(self, in_channels=20, seq_len=200):
-        super(Net, self).__init__()
+    def __init__(self, in_channels = 20,seq_len = 200,dropout=True):
+        super(Net,self).__init__()
         cnn1 = 64
         cnn2 = 128
         cnn3 = 128
+        self.dropout = dropout
         self.cnn1 = nn.Conv1d(in_channels, cnn1, kernel_size=3, padding=1)
-        self.normalization1 = nn.BatchNorm1d(cnn1)
-        self.pool1 = nn.MaxPool1d(2)
+        self.normalization1=nn.BatchNorm1d(cnn1)
+        self.pool1= nn.MaxPool1d(2)
         self.cnn2 = nn.Conv1d(cnn1, cnn2, kernel_size=3, padding=1)
-        self.normalization2 = nn.BatchNorm1d(cnn2)
-        self.pool2 = nn.MaxPool1d(2)
+        self.normalization2=nn.BatchNorm1d(cnn2)
+        self.pool2= nn.MaxPool1d(2)
         self.cnn3 = nn.Conv1d(cnn2, cnn3, kernel_size=3, padding=1)
-        self.normalization3 = nn.BatchNorm1d(cnn3)
-        self.pool3 = nn.MaxPool1d(2)
+        self.normalization3=nn.BatchNorm1d(cnn3)
+        self.pool3= nn.MaxPool1d(2)
         self.flatten = nn.Flatten()
 
         with torch.no_grad():
@@ -27,6 +30,7 @@ class Net(nn.Module):
             out = self.pool2(F.relu(self.cnn2(out)))
             out = self.pool3(F.relu(self.cnn3(out)))
             flat_dim = out.view(1, -1).size(1)
+
 
         self.snn1 = nn.Linear(flat_dim, 64)
         self.dropout1 = nn.Dropout(p=0.5)
@@ -50,24 +54,26 @@ class Net(nn.Module):
         x = self.flatten(x)
         x = self.snn1(x)
         x = F.relu(x)
-        x = self.dropout1(x)
+        if self.dropout:
+            x = self.dropout1(x)
         x = self.snn2(x)
         x = F.relu(x)
-        x = self.dropout2(x)
+        if self.dropout:
+            x = self.dropout2(x)
         out = self.snn3(x)
         return out
 
 
 class ModelInterface:
-    def __init__(self, model_path=r"models/mautner/cnn_model_3.pth"):
-        self.device = "cpu"
-        self.model = Net().to(device=self.device)
+    def __init__(self,model_path = r"models/mautner/cnn_model_augmentation_3.pth",dropout=False):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = Net(dropout=dropout).to(device=self.device)
         self.model.load_state_dict(torch.load(model_path, map_location=self.device))
-        self.log_mask = [0, 5, 10, 15]
+        self.log_mask = [0,5,10,15]
         self.model.eval()
-        self.target_shape = (1, 20, 200)  # [sample, kanaly, okno]
+        self.target_shape = (1,20,200) # [sample, kanaly, okno]
 
-    def predict(self, sample):
+    def predict(self,sample):
         """
         :param sample: musi mit shape (1,20,200) kde 20 je pocet vektoru a 200 je pocet prvku v jednom vektoru
         kanaly musi odpovidat:
@@ -78,25 +84,43 @@ class ModelInterface:
         :return: vraci predikci fáze
         """
 
-        sample = torch.tensor(sample, dtype=torch.float32).to(device=self.device)
-
+        sample = torch.tensor(sample, dtype=torch.float32).unsqueeze(0).to(device=self.device)
         if sample.shape != self.target_shape:
             if sample.shape[1] * sample.shape[2] == self.target_shape[1] * self.target_shape[2]:
-                sample = sample.permute(0, 2, 1)
+                sample = sample.permute(0,2,1)
             else:
-                raise Exception("Sample must have shape (1, 20, 200)")
-
-        sample[:, self.log_mask, :] = torch.log(torch.abs(sample[:, self.log_mask, :]))
+                raise Exception("Sample must have shape (1,20,200)")
+        sample[:,self.log_mask,:] = torch.log(torch.abs(sample[:,self.log_mask,:]))
         sample = self.normalize(sample)
-
         with torch.no_grad():
             output = self.model(sample)
             output_np = output.detach().cpu().numpy()
-            output_atan = np.arctan2(output_np[:, 0], output_np[:, 1]) / (2 * np.pi)
+            output_atan = np.arctan2(output_np[:,0],output_np[:,1]) / (2*np.pi)
             out = ((output_atan + 1) % 1.0) * 100
             return out
 
-    def normalize(self, signal):
+    def normalize(self,signal):
         mean = signal.mean(dim=2, keepdim=True)
         std = signal.std(dim=2, keepdim=True)
         return (signal - mean) / std
+
+
+
+if __name__ == "__main__":
+    import time
+    path = r"Y:\Datasets\Fyzio"
+    exercise = "Wide squat"
+    with open(os.path.join(path, "X_train_w_augmentation", exercise + ".pkl"), "rb") as f:
+        x = np.array(pickle.load(f))
+        print(x.shape)
+
+    with open(os.path.join(path, "Y_train_w_augmentation", exercise + ".pkl"), "rb") as f:
+        y = np.array(pickle.load(f))
+
+    start = time.time()
+    model = ModelInterface(model_path=r"models/cnn_model_augmentation_3.pth",dropout=False)
+    for i in range(1000):
+        output = model.predict(x[i])
+        print(output, y[i], np.abs(output-y[i]))
+    time_end = time.time()
+    print(time_end - start)
